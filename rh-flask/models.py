c9958613de1b1408449a4d2e158be3e0ddfdc594
jsong <jeisongg02@gmail.com>
2025-11-12 20:10:57 -0500

    Ajuste para hash de contrasema

--- rh-flask/models.py
+++ rh-flask/models.py
@@ -1,4 +1,3 @@
-# models.py
 from extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import Numeric
@@ -9,11 +8,16 @@ class Empleado(db.Model):
     idEmpleado = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(120), nullable=False)
     departamento = db.Column(db.String(100), nullable=False)
-    sueldo = db.Column(Numeric(10, 2), nullable=False)  # precision 10, 2 decimales
+    sueldo = db.Column(Numeric(10, 2), nullable=False)
     correo = db.Column(db.String(200), unique=True, nullable=False)
     _contrasena = db.Column("contrasena", db.String(200), nullable=False)
 
-    def set_password(self, raw_password: str):
+    @property
+    def contrasena(self):
+        raise AttributeError("La contraseña no puede leerse directamente.")
+
+    @contrasena.setter
+    def contrasena(self, raw_password):
         self._contrasena = generate_password_hash(raw_password)
 
     def check_password(self, raw_password: str) -> bool:
